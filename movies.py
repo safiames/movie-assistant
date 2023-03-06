c34cd303ce2c98bdad3767648675ee6610b591d6
Safia Messaoud <81869764+safiames@users.noreply.github.com>
2023-03-06 20:16:19 +0200

    Created a new file and imported from there

--- movies.py
+++ movies.py
@@ -1,22 +1,5 @@
 import random
-
-movie_list = ["2gether: The Movie", "Love of Siam", "Yes Or No", "Tootsies and the Fake", "Ghost Lab"]
-
-movie_summaries = {
-    "2gether: The Movie": "The film follows the relationship between Tine and Sarawat, two college students who pretend to be in a relationship to win a music competition. As they spend more time together, their feelings for each other become real, but their newfound love is put to the test when they face challenges from their friends, family, and the media.",
-    "Love of Siam": "A Thai romantic drama film that tells the story of two childhood friends, Mew and Tong, who are reunited after years apart. As they reconnect, they must navigate their feelings for each other while also dealing with personal issues, family problems, and a mysterious disappearance.",
-    "Yes Or No": "Romantic comedy-drama film that tells the story of Pai, a college girl who is openly gay, and her new roommate, Kim, who is initially uncomfortable with Pai's sexuality. As they spend more time together, however, they begin to develop feelings for each other, leading to a complicated relationship. ",
-    "Tootsies and the Fake": "Comedy film that tells the story of four gay friends - Song, Kanda, Pong, and Pongsatorn who try to expose a fraudulent psychic medium who has swindled their boss. In order to infiltrate the psychic's inner circle, the friends create new identities as a wealthy family and their servants. As they carry out their plan, they face unexpected challenges and comedic mishaps.",
-    "Ghost Lab": "A Thai horror film that follows two doctors, Wee and Gla, who become obsessed with proving the existence of ghosts after experiencing a paranormal encounter during a hospital emergency. They conduct a series of experiments to document and analyze supernatural phenomena, which leads them to confront their own personal demons and the consequences of their actions. "
-}
-
-movie_genres = {
-    "2gether: The Movie": ["romance", "comedy"],
-    "Love of Siam": ["mystery", "Thai"],
-    "Yes Or No": ["LGBTQ", "Thai", "romance"],
-    "Tootsies and the Fake": ["comedy", "Thai"],
-    "Ghost Lab": ["supernatural", "horror"]
-}
+from movie_library import movie_list, movie_summaries, movie_genres
 
 # create a list of all the movie genres
 all_genres = sum(movie_genres.values(), [])
